e9bf16fd84aeba17bc673fc06a59c28d73f637eb
hwyncho <hwiyeon.cho@makinarocks.ai>
2022-03-28 16:23:00 +0900

    :wrench: Enable termguicolors in kitty

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,7 @@
 if string.find(os.getenv("TERM"), "256color") then
     vim.opt["termguicolors"] = true
+elseif string.find(os.getenv("TERM"), "kitty") then
+    vim.opt["termguicolors"] = true
 end
 
 vim.opt["background"] = "dark"
