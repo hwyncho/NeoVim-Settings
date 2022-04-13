da8ac856076f2f884206c310eae02671f2ca2835
hwyncho <hwiyeon.cho@makinarocks.ai>
2022-04-13 11:43:47 +0900

    :wrench: Add settings for yaml-language-server

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -2,7 +2,11 @@ local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status_ok then return end
 
 local setup = {
-    ensure_installed = "maintained",
+    ensure_installed = {
+        "bash", "css", "dockerfile", "javascript", "json", "json5", "jsonc",
+        "lua", "markdown", "go", "make", "python", "toml", "tsx", "typescript",
+        "vim", "vue", "yaml"
+    },
     sync_install = false,
     ignore_install = {""},
     autopairs = {enable = true},
